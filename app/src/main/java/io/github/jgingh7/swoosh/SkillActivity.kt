fafc19ddced712787591b8ae092110c09dbdbca2
Jin Gyu Lee <ccjglingm07@gmail.com>
2020-05-28 00:01:52 -0500

    - fixed margins on bottom buttons - fixed capitalization for toggle buttons - made "skill" layout

---
+++ app/src/main/java/io/github/jgingh7/swoosh/SkillActivity.kt
@@ -0,0 +1,12 @@
+package io.github.jgingh7.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
